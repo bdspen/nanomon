2b8e0f615e2cd3bd468cc4bed196cb0e593b09f1
Benjamin Spenard <ben@Benjamins-MacBook-Pro.local>
2016-04-16 23:10:25 -0700

    add memory mon and system cpu

--- app.js
+++ app.js
@@ -5,16 +5,41 @@ var usage = require('os-usage');
 var cpuMonitor = new usage.CpuMonitor();
 
 //elements-----------------------
-var cpuEm = document.getElementById('cpu-container');
+var ems = {};
+ems.user = document.getElementById('user-container');
+ems.sys = document.getElementById('sys-container');
+
 
 //functions-----------------------
 cpuMonitor.on('cpuUsage', function(data){
-  cpuEm.innerHTML = "CPU Usage: " + data.user + "%";
-  style(cpuEm, data.user, "#DD4814");
+  ems.user.innerHTML = "User: " + data.user + "%";
+  ems.sys.innerHTML = "System: " + data.sys + "%";
+  data.userColor = "#3862e0";
+  data.sysColor = "#DD4814";
+  for(var key in ems){
+    style(key, data[key], data[key + "Color"]);
+  }
 });
 
+
+var memMonitor = new usage.MemMonitor();
+memMonitor.on('topMemProcs', function(data) {
+    console.log(data);
+    for(var i = 0; i < data.length; i++){
+      // createProcessList(data[i]);
+    }
+
+    // [ { pid: '0', mem: '1521M', command: 'kernel_task' } ]
+});
+
+var createProcessList = function(data){
+  var processes = document.getElementById("processes");
+  var li = document.createElement("li");
+  processes.appendChild(li);
+  li.innerHTML = data.command + ": memory usage: " + data.mem;
+}
+
 var style = function(em, value, color){
-    em.style["width"] = value + '%';
-    em.style["background-color"] =  color;
-    em.style["border-radius"] = "5px";
+    ems[em].style["width"] = value + '%';
+    ems[em].style["background-color"] =  color;
 };
--- main.js
+++ main.js
@@ -2,9 +2,9 @@ var menubar = require('menubar')
 
 var opts = {
     width: 302,
-    height: 22,
+    height: 41,
     "window-position": "topRight",
-    resizable: false,
+    resizable: true,
     backgroundColor: '#333333',
     icon: __dirname + '/ic_timeline_white_24dp_1x.png',
     "always-on-top": true
@@ -14,5 +14,5 @@ var mb = menubar(opts);
 
 mb.on('after-create-window', function ready() {
 
-  // mb.window.openDevTools()
+  mb.window.openDevTools()
 });
