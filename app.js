c76202e7e31412a032f09fbe171e1fd81fb9d199
Benjamin Spenard <ben@Benjamins-MacBook-Pro.local>
2016-04-16 18:52:12 -0700

    add menubar dependency to clean up project, remove memory logging

--- app.js
+++ app.js
@@ -1,44 +1,20 @@
 'use-strict';
 
 //dependencies--------------------
-var os = require('os-utils');
+var usage = require('os-usage');
+var cpuMonitor = new usage.CpuMonitor();
 
 //elements-----------------------
-var cpuEm = document.getElementById('cpu');
-var memoryPercentEm = document.getElementById('memory-percent');
-var freeMemEm = document.getElementById('freemem');
+var cpuEm = document.getElementById('cpu-container');
 
 //functions-----------------------
-var cpuMon = function() {
-    os.cpuUsage(function(v) { //will return cpu usage in % over 1s
-        var cpuPercent = (v * 100).toFixed(2);
-        // console.log(cpuPercent);
-        cpuEm.innerHTML = "CPU USAGE: " + cpuPercent + "%";
-        style(cpuEm, cpuPercent, "#DD4814");
-    })
-}
-
-var memoryPercentMon = function(){
-    var memoryPercent = Math.abs((os.freememPercentage() * 100).toFixed(2)- 100);
-    var freeMem = (os.freemem() / 1024).toFixed(2);
-    // console.log(memoryPercent);
-    memoryPercentEm.innerHTML = "MEMORY USAGE: " + memoryPercent + " %" + " / FREE: " + freeMem + " GB";
-    style(memoryPercentEm, memoryPercent, "#77216F");
-}
+cpuMonitor.on('cpuUsage', function(data){
+  cpuEm.innerHTML = "CPU Usage: " + data.user + "%";
+  style(cpuEm, data.user, "#DD4814");
+});
 
 var style = function(em, value, color){
     em.style["width"] = value + '%';
     em.style["background-color"] =  color;
-    em.style["border-radius"] = "6px";
-}
-var bytesToSize = function (bytes) {
-    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes == 0) return 'n/a';
-    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-    if (i == 0) return bytes + ' ' + sizes[i];
-    return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+    em.style["border-radius"] = "5px";
 };
-
-//updates the monitored values per unit of time.
-setInterval(cpuMon, 1000);
-setInterval(memoryPercentMon, 2000);
--- main.js
+++ main.js
@@ -1,46 +1,18 @@
-var app = require('app'); // Module to control application life.
-var util = require('util'); //node utilities
-var BrowserWindow = require('browser-window'); // Module to create native browser window.
-var Menu = require('menu'); // used to create native menus that can be used as application menus and context menus.
-var Tray = require('tray'); //operating system's notification area
+var menubar = require('menubar')
 
-var appIcon = null; //empty tray icon
+var opts = {
+    width: 302,
+    height: 22,
+    "window-position": "topRight",
+    resizable: false,
+    backgroundColor: '#333333',
+    icon: __dirname + '/ic_timeline_white_24dp_1x.png',
+    "always-on-top": true
+};
 
-app.on('ready', function ready() {
-    var contextMenu = Menu.buildFromTemplate([{
-        label: 'Monitor',
-        click: function() {
-            if (mainWindow.isVisible()) {
-                mainWindow.hide();
-            } else {
-                mainWindow.show();
-            }
-        }
-    }]);
+var mb = menubar(opts);
 
-    //build the window
-    mainWindow = new BrowserWindow({
-        width: 304,
-        height: 68,
-        x: 9999,
-        y: 0,
-        backgroundColor: '#333333',
-        transparent: true,
-        frame: false,
-        show: true,
-        icon: __dirname + '/neutron.png'
-    });
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+mb.on('after-create-window', function ready() {
 
-    //build icon and tray
-    appIcon = new Tray('neutron.png');
-    appIcon.setContextMenu(contextMenu);
-
-    //open devtools
-    mainWindow.webContents.openDevTools();
-
-    //on window close, shut the app
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
+  // mb.window.openDevTools()
 });
