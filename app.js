d46de151551209549eaadafa9fef72158073c2ec
Ben Spenard <bdspen@gmail.com>
2016-04-21 21:10:25 -0700

    edit styles, add kill process functionality

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 var usage = require('os-usage');
 var cpuMonitor = new usage.CpuMonitor();
 var ipc = require('electron').ipcRenderer;
+var spawn = require('child_process').spawn;
+var opts = {limit: 19};//options for memMonitor and CpuMonitor
 
 //elements-----------------------
 var ems = {};
@@ -13,22 +15,21 @@ ems.processes = document.getElementById("processes");
 
 
 //functions-----------------------
-cpuMonitor.on('cpuUsage', function(data){
-  ems.user.innerHTML = "User: " + data.user + "%";
-  ems.sys.innerHTML = "System: " + data.sys + "%";
+cpuMonitor.on('cpuUsage', function(data){//monitors the Cpu
+  ems.user.innerHTML = "User: " + data.user + "%";//user cpu usage
+  ems.sys.innerHTML = "System: " + data.sys + "%";//system cpu usage
   data.userColor = "#3862e0";
   data.sysColor = "#DD4814";
-  for(var key in ems){
+  for(var key in ems){//for every iteration, animate the container bars
     style(key, data[key], data[key + "Color"]);
   }
 });
 
-
-var memMonitor = new usage.MemMonitor({limit: 10});
+var memMonitor = new usage.MemMonitor(opts);//monitors processes and memory
 memMonitor.on('topMemProcs', function(data) {
-    // console.log(data);
+    //checks to see if there is a list before initailizing list
     if(ems.processes.hasChildNodes() === false){
-      initList(data);
+      initList(data);//creates the list of html elements
     }if(ems.processes.hasChildNodes() === true){
       for(var i = 0; i < data.length; i++){
         ems.processes.childNodes[i].innerHTML = data[i].command + ": memory usage: " + data[i].mem;
@@ -38,19 +39,35 @@ memMonitor.on('topMemProcs', function(data) {
 });
 
 var initList = function(data){
+  //for every process create a li with specific id, class "process" and click event to kill
   for (var i = 0; i < data.length; i++) {
     var li = document.createElement("li");
-    li.setAttribute("id", "process" + i);
-    li.setAttribute("class", "process")
+    li.setAttribute("id", data[i].pid);
+    li.setAttribute("class", "process");
+    li.onclick = function(){
+      killProcess(this.id)
+    }
     ems.processes.appendChild(li);
   }
 }
 
+//animates the width of the cpu monitor (container) bars
 var style = function(em, value, color){
     ems[em].style["width"] = value + '%';
     ems[em].style["background-color"] =  color;
 };
 
+//ask for confirmation and kill process if confirmed
+var killProcess = function(pid){
+  var confirmed = window.confirm("Are you sure you want to kill process: " + pid + "?");
+  if(confirmed == true){
+    var kill = spawn('kill', [pid]);
+  }else{
+    return;
+  }
+}
+
+//sends a message to the main.js file to expand the window size
 var expand = function(){
   if(window.innerHeight <= 68){
     ipc.sendSync('synchronous-message', 'expand');
--- readme.md
+++ readme.md
@@ -6,15 +6,15 @@
 
 ## Description
 
-_A small tool that allows you to keep track of the load on your system's CPU and Memory_
+_A small menubar app that allows you to keep track of the load on your system's CPU, Memory and Processes_
 
 ## Installation
 
-At this early stage of development, If you would like to run the program, clone this repo, run `npm install`, navigate to the project directory and run: `electron .`
+If you would like to run the program, clone this repo, run `npm install`, navigate to the project directory and run: `electron .`
 
 ## bugs / to-do
 * test on other platforms
-* The memory usage monitor currently displays the memory usage _including memory that can be made available to the running processes._ To put that another way, the memory usage bar may show that your machine is using 90% when actually most of the memory use is for buffers and cache. Linux will free up the buffers and cache to yield memory for other applications, so this memory report is not exactly the report I would like.
+* test and optimize performance
 
 ## Contact Details
 
